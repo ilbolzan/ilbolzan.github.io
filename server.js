4b28064f32be309cac380a9d2cfbdc76cf821da6
Yuri Daniel Vieira Mostardeiro <YVMostardeiro@senacrs.com.br>
2019-05-28 17:37:32 -0300

    ajuste para projeto rodar

--- server.js
+++ server.js
@@ -4,27 +4,22 @@ const upload = multer({dest: __dirname + '/uploads/images'});
 
 const app = express();
 const PORT = 1080;
-
-app.use(express.static('public'));
 var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+//app.use(express.bodyParser()); // Automatically parses form dat
 app.use(bodyParser.json()); // support json encoded bodies
-app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
-app.post('/upload', function(req, res) {
-    var id = req.body.id;
-    var photo = req.body.photo;
 
-    res.send(id + ' ' + photo);
-});
-
-/*app.post('/upload', upload.single('photo'), (req, res) => {
+app.post('/upload',  (req, res) => {
     res.json(req.body.photo);
-    if(req.file) {
+    /*if(req.file) {
         res.json(req.file);
     }
-    else throw 'error';
-});*/
-
+    else throw 'error';*/
+});
+//
 app.listen(PORT, () => {
     console.log('Listening at ' + PORT );
 });
